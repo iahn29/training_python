946e29b95b404e4ac6422eecbb414289f04c4e5d
iahn29 <iahn@free.fr>
2019-09-07 21:20:58 +0200

    Ajouté la gestion des fichiers json

--- san_antonio.py
+++ san_antonio.py
@@ -1,13 +1,14 @@
 # -*- coding: utf8 -*-
 
 import random
+import json
 
-quotes = [
+'''quotes = [
     "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !", 
     "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
-]
+]'''
 
-characters = [
+'''characters = [
     "alvin et les Chipmunks", 
     "Babar", 
     "betty boop", 
@@ -15,12 +16,30 @@ characters = [
     "casper", 
     "le chat potté", 
     "Kirikou"
-]
+]'''
 
+def read_value_from_json(file_name,key):
+  values = []
+  # Open the json file
+  with open(file_name) as f:
+    # Load file data in data
+    data = json.load(f)
+    for entry in data:
+      values.append(entry[key])
+    return values
+   
 def get_random_item_in(my_list):
   rand_index = random.randint(0, len(my_list) - 1) # random index
   item = my_list[rand_index] # get a quote from the list
   return(item) #r return the item
+
+def get_random_character():
+  characters = read_value_from_json("characters.json","character")
+  return get_random_item_in(characters)
+
+def get_random_quote():
+  quote = read_value_from_json("quotes.json","quote")
+  return get_random_item_in(quote)
 
 def message(character, quote):
   n_character=character.capitalize()
@@ -30,5 +49,6 @@ def message(character, quote):
 user_answer = "" 
 
 while user_answer != "q":
-  print(message(get_random_item_in(characters),get_random_item_in(quotes)))
+  print(message(get_random_character(),get_random_quote()))
   user_answer = input('Tapez entrée pour connaitre une autre citation ou q pour quitter le programme.') 
+
